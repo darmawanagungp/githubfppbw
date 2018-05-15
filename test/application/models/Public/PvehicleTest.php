8d8a422200c625594e83817b7277fbb09fc3f635
siscaagatha <sisca356@gmail.com>
2018-05-15 22:39:34 +0700

    Testing1

---
+++ test/application/models/Public/PvehicleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PvehicleTest
+ *
+ * @author Lenovo
+ */
+class PvehicleTest extends PHPUnit_Framework_TestCase {
+
+    public function testSimple() {
+        $this->assertEquals(1,1);
+    }
+}
